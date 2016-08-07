7f4945e48938484f30339566586234f1e7857e0a
Kristiyan Peychev <kurotsuki_l@yahoo.com>
2016-08-07 14:39:30 +0300

    Removed platform-specific Sleep function

--- main.cpp
+++ main.cpp
@@ -4,15 +4,17 @@
 #include "stdafx.h"
 #include "service_provider.h"
 #include "account.h"
-#include "Windows.h"
+//#include "Windows.h"
 
 int main()
 {
     service_provider prov;
-    account acc1(&prov, phone("1234"));
-    account acc2(&prov, phone("05012342"));
+    phone acc1_phone = "1234";
+    phone acc2_phone = "050123421234";
+    account acc1(&prov, acc1_phone);
+    account acc2(&prov, acc2_phone);
     auto first = std::chrono::system_clock::now();
-    Sleep(2);
+  //  Sleep(2);
     auto *EMONE = acc1.make_call(acc2.get_personal_number(), first, std::chrono::system_clock::now());
     EMONE->get_amount_in_dollars();
 
